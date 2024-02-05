bac0659b4322970c321724bf3836fa67ea810eaa
jasontong <jasontong1209@gmail.com>
2024-02-05 16:50:49 -0500

    file upload

---
+++ Resources/crowdfunding_db.sql
@@ -0,0 +1,13 @@
+-- Verify that each table has the correct data by running a SELECT statement for each.
+
+Select *
+FROM campaign
+
+Select *
+From category
+
+Select *
+From contacts
+
+Select *
+From subcategory
---
+++ crowdfunding_db.sql
@@ -0,0 +1,14 @@
+--Loaded tables in order of subcategory, contacts, category, campaign 
+--Verified that each table has the correct data by running a SELECT statement for each.
+
+Select *
+FROM campaign
+
+Select *
+From category
+
+Select *
+From contacts
+
+Select *
+From subcategory
